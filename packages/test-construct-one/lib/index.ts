456cbecaab1608226585892ad178fb385a5f325b
medelman17 <mike.edelman@gmail.com>
2021-12-13 20:08:34 -0500

    build: test lint-staged

--- packages/test-construct-one/lib/index.ts
+++ packages/test-construct-one/lib/index.ts
@@ -1,12 +1,13 @@
-import { Construct } from 'constructs';
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export interface TestConstructOneProps {
   // Define construct properties here
 }
 
-export class TestConstructOne extends Construct {
+export const HELLO = "hello";
 
+export class TestConstructOne extends Construct {
   constructor(scope: Construct, id: string, _: TestConstructOneProps = {}) {
     super(scope, id);
 
